a258f0ea7d7e3e1cba76e785f526c3a680537153
gitkcb <kcharlesbell@gmail.com>
2022-12-20 18:59:18 -0500

    worked on models

--- models/Gallery.js
+++ models/Gallery.js
@@ -3,6 +3,40 @@ const sequelize = require("../config/connection");
 class Gallery extends Model {}
 
 Gallery.init(
-    {}
-)
+    {
+        id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type:DataType.STRING,
+        },
+        date_created: {
+            type: DataType.DATE,
+            allowNull: false,
+            defaultValue: DataType.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER, 
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+
+
+    },
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName: true,
+        modelName: 'gallery',
+    }
+);
 module.exports = Gallery;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User');
 const Gallery = require('./Gallery');
 
+User.hasMany(Gallery, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+Gallery.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Gallery };
