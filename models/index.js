a6ec85b3dd788f67b34443ed6fab83eed0ebbab7
gitkcb <kcharlesbell@gmail.com>
2022-12-19 21:25:29 -0500

    user model update

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Gallery = require('./Gallery');
 
+User.hasMany (Gallery, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Gallery.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Gallery };
