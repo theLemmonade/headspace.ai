1416e6ba9251542d11e8d8a378917ad9609cb8b8
EmilyBernard <bernam2000@hotmail.com>
2022-12-20 17:49:49 -0500

    added helpers const in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
