9c272f7abc632a6ed42e1951d7329056d1a31b2a
gitkcb <kcharlesbell@gmail.com>
2022-12-21 14:11:46 -0500

    linked html routes

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,42 +1,42 @@
-const { Model, DataTypes} = require('sequelize');
-const sequelize = require("../config/connection");
-class Gallery extends Model {}
+// const { Model, DataTypes} = require('sequelize');
+// const sequelize = require("../config/connection");
+// class Gallery extends Model {}
 
-Gallery.init(
-    {
-        id: {
-            type: DataType.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type:DataType.STRING,
-        },
-        date_created: {
-            type: DataType.DATE,
-            allowNull: false,
-            defaultValue: DataType.NOW,
-        },
-        user_id: {
-            type: DataTypes.INTEGER, 
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
+// Gallery.init(
+//     {
+//         id: {
+//             type: DataType.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+//         name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         description: {
+//             type:DataType.STRING,
+//         },
+//         date_created: {
+//             type: DataType.DATE,
+//             allowNull: false,
+//             defaultValue: DataType.NOW,
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER, 
+//             references: {
+//                 model: 'user',
+//                 key: 'id',
+//             },
+//         },
 
 
-    },
-    {
-        sequelize,
-        timestamps:false,
-        freezeTableName: true,
-        modelName: 'gallery',
-    }
-);
-module.exports = Gallery;
+//     },
+//     {
+//         sequelize,
+//         timestamps:false,
+//         freezeTableName: true,
+//         modelName: 'gallery',
+//     }
+// );
+// module.exports = Gallery;
