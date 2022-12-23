bfa4d6df9e89b031980ea059f9ab978c46bf5e2b
EmilyBernard <bernam2000@hotmail.com>
2022-12-22 19:00:42 -0500

    added and coded seed file in seeds folder

---
+++ seeds/seed.js
@@ -0,0 +1,15 @@
+const sequelize = require('../config/connection');
+const seedImage= require('./imageData');
+const seedUser = require('./userData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedImage();
+
+  await seedUser();
+
+  process.exit(0);
+};
+
+seedAll();
