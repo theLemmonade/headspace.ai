6c8bc2a85cc3f8e5debd1b714609a6964d2c1d65
Alex Frankhouser <freesoul423@gmail.com>
2022-12-22 19:55:13 -0500

    cleaned up server.js code

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
-// const path = require("path");
+const path = require("path");
 const express = require("express");
 const dotenv = require('dotenv').config();
-// const exphbs = require("express-handlebars");
-// // Import express-session
-// const session = require('express-session');
+const exphbs = require("express-handlebars");
+// Import express-session
+const session = require('express-session');
 
 const routes = require("./controllers");
-// const sequelize = require("./config/connection");
-// const helpers = require('./utils/helpers');
+const sequelize = require("./config/connection");
+const helpers = require('./utils/helpers');
 
 const app = express();
 // requires the openai route for future coding or additions.
-// app.use('/openai', require('./controllers/api/openaiRoutes'))
+app.use('/openai', require('./controllers/api/openaiRoutes'))
 
 // Enable body parser for openai
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
+
 // // Set up sessions
-// const sess = {
-//     secret: 'Super secret secret',
-//     resave: false,
-//     saveUninitialized: false,
-//   };
-  
-//   app.use(session(sess));
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: false,
+    };
 
-// const hbs = exphbs.create({});
+    app.use(session(sess));
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+const hbs = exphbs.create({});
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-// app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log("Now listening"));
-// });
+sequelize.sync({ force: false })
 
