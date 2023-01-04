e215c5aa293fa1310106e2c61d8ebb3ace2a9530
gitkcb <kcharlesbell@gmail.com>
2023-01-03 19:26:30 -0500

    usergallery route work

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,43 +1,43 @@
-
-const router = require('express').Router();
-const {Image, User, UserGallery } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Image, User, UserGallery } = require("../models");
+const withAuth = require("../utils/auth");
 //GET request for homepage.
-router.get('/', (req, res) => {
-    res.render('homepage')});
-//GET request for gallery
-router.get('/gallery', (req, res) => {
-
-
-    /// get the db data that gallery is going to use 
-    res.render('gallery')});
-//GET request for login
-router.get('/login', (req, res) => {
-//If user is logged in, user will be redirected usergallery page. 
-    if (req.session.logged_in) {
-        res.redirect('/usergallery');
-        return;
-      }
-    res.render('login')});
-//GET request for registry
-router.get('/register', (req, res) => {
-    res.render('register')});
-//GET request for user gallery
-router.get('/usergallery', withAuth,async (req, res) => {
-   
-    try {
-        const userGalleryData= await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{model: UserGallery}],
-        });
-    const userGallery = userGalleryData.get({ plain: true});
-    
-    res.render('user-gallery', {
-        ...userGallery,
-        logged_in: true
-    });
-}catch(err) {
-        res.status(500).json(err);
-}
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
-module.exports = router
+//GET request for gallery
+router.get("/gallery", (req, res) => {
+  /// get the db data that gallery is going to use
+  res.render("gallery");
+});
+//GET request for login
+router.get("/login", (req, res) => {
+  //If user is logged in, user will be redirected usergallery page.
+  if (req.session.logged_in) {
+    res.redirect("/usergallery");
+    return;
+  }
+  res.render("login");
+});
+//GET request for registry
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+//GET request for user gallery
+router.get("/usergallery/:id", withAuth, async (req, res) => {
+  try {
+    const userGalleryData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: UserGallery }],
+    });
+    const userGallery = userGalleryData.get({ plain: true });
+
+    res.render("user-gallery", {
+      ...userGallery,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
