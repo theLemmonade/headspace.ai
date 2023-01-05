80a2ab662d77c4c80fbd78903030c65622fccacd
theLemmonade <lemmonmail@icloud.com>
2023-01-05 13:23:58 -0500

    finished login.html and began login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,45 @@
 const regNav = document.querySelector("#regNav");
-const regEl = document.querySelector("#regEl")
+const regEl = document.querySelector("#regEl");
 const toReg = document.querySelector("#toReg");
 const regUser = document.querySelector("#regUser");
 const regEmail = document.querySelector("#regEmail");
 const regPass = document.querySelector("#regPass");
 const regBtn = document.querySelector("#regBtn");
 const logNav = document.querySelector("#logNav");
-const logEl = document.querySelector("#logEl")
+const logEl = document.querySelector("#logEl");
 const toLog = document.querySelector("#toLog");
 const logUser = document.querySelector("#logUser");
 const logPass = document.querySelector("#logPass");
 const logBtn = document.querySelector("#logBtn");
 
-function showReg(){
-    regNav.setAttribute("class", "nav-link active")
-    regNav.setAttribute("aria-current", "true")
-    regEl.setAttribute("style", "display: block;")
-    logNav.setAttribute("class", "nav-link")
-    logNav.setAttribute("aria-current", "false")
-    logEl.setAttribute("style", "display: none;")
-    console.log("display register");
+function showReg() {
+	regNav.setAttribute("class", "nav-link active");
+	regNav.setAttribute("aria-current", "true");
+	regEl.setAttribute("style", "display: block;");
+	logNav.setAttribute("class", "nav-link");
+	logNav.setAttribute("aria-current", "false");
+	logEl.setAttribute("style", "display: none;");
+	console.log("display register");
 }
 
-function showLog(){
-    regNav.setAttribute("class", "nav-link")
-    regNav.setAttribute("aria-current", "false")
-    regEl.setAttribute("style", "display: none;")
-    logNav.setAttribute("class", "nav-link active")
-    logNav.setAttribute("aria-current", "true")
-    logEl.setAttribute("style", "display: block;")
-    console.log("display login")
+function showLog() {
+	regNav.setAttribute("class", "nav-link");
+	regNav.setAttribute("aria-current", "false");
+	regEl.setAttribute("style", "display: none;");
+	logNav.setAttribute("class", "nav-link active");
+	logNav.setAttribute("aria-current", "true");
+	logEl.setAttribute("style", "display: block;");
+	console.log("display login");
 }
 
-function register(){
-    console.log("register go")
-    // TODO register logic
+function register() {
+	console.log("register go");
+	// TODO register logic
 }
 
-function login(){
-    console.log("login go")
-    // TODO login logic
+function login() {
+	console.log("login go");
+	// TODO login logic
 }
 
 showReg();
