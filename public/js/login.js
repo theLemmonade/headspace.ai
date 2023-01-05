81a62dd75481b6b0646d92fdaab7f740dc7442f5
theLemmonade <lemmonmail@icloud.com>
2023-01-05 12:25:32 -0500

    created login.js and assigned query selectors

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const regNav = document.querySelector("#regNav");
+const toReg = document.querySelector("#toReg");
+const regUser = document.querySelector("#regUser");
+const regEmail = document.querySelector("#regEmail");
+const regPass = document.querySelector("#regPass");
+const regBtn = document.querySelector("#regBtn");
+const logNav = document.querySelector("#logNav");
+const toLog = document.querySelector("#toLog");
+const logUser = document.querySelector("#logUser");
+const logPass = document.querySelector("#logPass");
+const logBtn = document.querySelector("#logBtn");
+
+function showReg(){
+
+}
+
+function showLog(){
+
+}
+
+function register(){
+// TODO register logic
+
+}
+
+function login(){
+// TODO login logic
+}
