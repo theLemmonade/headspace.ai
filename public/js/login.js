acf4281550cd134c6ee6d933e5c30657818ff5a2
theLemmonade <lemmonmail@icloud.com>
2023-01-05 17:42:10 -0500

    added login.js api logic

--- public/js/login.js
+++ public/js/login.js
@@ -8,10 +8,11 @@ const regBtn = document.querySelector("#regBtn");
 const logNav = document.querySelector("#logNav");
 const logEl = document.querySelector("#logEl");
 const toLog = document.querySelector("#toLog");
-const logUser = document.querySelector("#logUser");
+const logEmail = document.querySelector("#logEmail");
 const logPass = document.querySelector("#logPass");
 const logBtn = document.querySelector("#logBtn");
 
+// show the register section
 function showReg() {
 	regNav.setAttribute("class", "nav-link active");
 	regNav.setAttribute("aria-current", "true");
@@ -22,6 +23,7 @@ function showReg() {
 	console.log("display register");
 }
 
+// show the login section
 function showLog() {
 	regNav.setAttribute("class", "nav-link");
 	regNav.setAttribute("aria-current", "false");
@@ -32,21 +34,57 @@ function showLog() {
 	console.log("display login");
 }
 
-function register() {
-	console.log("register go");
-	// TODO register logic
-}
-
-function login() {
-	console.log("login go");
-	// TODO login logic
-}
-
-// Choose default:
-//showReg();
+// Choose a default section, and display it
 showLog();
 
-regNav.addEventListener("click", showReg);
-toReg.addEventListener("click", showReg);
-logNav.addEventListener("click", showLog);
-toLog.addEventListener("click", showLog);
+const loginFormHandler = async (event) => {
+	event.preventDefault();
+
+	// Collect values from the login form
+	const email = logEmail.value.trim();
+	const password = logPass.value.trim();
+
+	if (email && password) {
+		// Send a POST request to the API endpoint
+		const response = await fetch("/api/user/login", {
+			method: "POST",
+			body: JSON.stringify({ email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			// If successful, redirect the browser to the profile page
+			document.location.replace("/usergallery");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	// Collect values from the register form
+	const user_name = regUser.value.trim();
+	const email = regEmail.value.trim();
+	const password = regPass.value.trim();
+
+	if (user_name && email && password) {
+		// Send a POST request to the API endpoint
+		const response = await fetch("/api/user", {
+			method: "POST",
+			body: JSON.stringify({ user_name, email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			// If successful, redirect the browser to the profile page
+			document.location.replace("/");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+logBtn.addEventListener("click", loginFormHandler);
+regBtn.addEventListener("click", signupFormHandler);
