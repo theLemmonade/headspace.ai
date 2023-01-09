5f25e1303dd30232be9119b308222a0093bd4288
theLemmonade <lemmonmail@icloud.com>
2023-01-09 11:17:18 -0500

    bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -4,23 +4,35 @@ const resultPrompt = document.querySelector("#resultPrompt");
 const resultUser = document.querySelector("#resultUser");
 const resultDate = document.querySelector("#resultDate");
 const resultShareIcon = document.querySelector("#resultShareIcon");
-const resultModal = new bootstrap.Modal(document.querySelector("#resultModal"),{ keyboard: false });
-const errModal = new bootstrap.Modal(document.querySelector("#errModal"), {keyboard: false,});
+const resultDl = document.querySelector("#resultDl");
+const resultModal = new bootstrap.Modal(
+  document.querySelector("#resultModal"),
+  { keyboard: false }
+);
+const errModal = new bootstrap.Modal(document.querySelector("#errModal"), {
+  keyboard: false,
+});
+// TODO assign these to lets to using session data
+// let user_name = String;
+// let date_created = String;
 let imageURL = String;
 let promptValue = String;
 let isPrivate = Boolean;
 imageURL = "./assets/1024x1024.png";
 isPrivate = true;
+console.log("index.js");
 
 // Hide spinner
-document.querySelector("#spinner").setAttribute("class", "spinner-border text-warning d-none");
+document
+  .querySelector("#spinner")
+  .setAttribute("class", "spinner-border text-warning d-none");
 console.log("hiding spinner");
 
 function onSubmit(event) {
   event.preventDefault();
   console.log("onSubmit go");
   promptValue = promptInput.value;
-  console.log("prompt is " + promptValue);
+  console.log(promptValue);
   // If nothing is added then an alert pops up
   if (promptValue === "") {
     document
@@ -58,17 +70,17 @@ async function generateImageRequest() {
         .setAttribute("class", "spinner-border text-warning d-none");
       console.log("hiding spinner");
       throw new Error("405, that image could not be generated");
-    } 
+    }
     const data = await response.json();
     console.log("data, ", data);
     imageURL = data.data;
     document.querySelector("#resultImg").src = imageURL;
+
     configModal();
     beginModal();
   } catch (error) {
     beginErrModal(error);
   }
-
 }
 
 // config Modal
@@ -76,7 +88,13 @@ function configModal() {
   console.log("configModal go");
   // set modal content from API response and session data
   resultImg.setAttribute("src", imageURL);
+  resultDl.setAttribute("href", imageURL);
   resultPrompt.textContent = promptValue;
+  // TODO get session data and store under 'resultUser'
+  // resultUser.textContent = user_name;
+  // console.log(user_name);
+  // TODO get current date and store under 'resultDate'
+  // resultDate.textContent = date_created;
 }
 
 // launch Modal
@@ -88,25 +106,6 @@ function beginModal() {
     .setAttribute("class", "spinner-border text-warning d-none");
   console.log("hiding spinner");
 }
-
-async function newImageHandler() {
-  if (imageURL && promptValue) {
-    const response = await fetch("/api/usergallery", {
-      method: "POST",
-      body: JSON.stringify({ imageURL, promptValue }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      document.location.replace("/userGallery");
-    } else {
-      alert("Failed to create image");
-    }
-    console.log(response);
-  }
-}
-
 
 // toggle isPrivate
 function publishBtn() {
@@ -127,17 +126,56 @@ function beginErrModal(error) {
   errModal.show();
 }
 
+async function newImageHandler() {
+  if (imageURL && promptValue) {
+    const response = await fetch("/api/usergallery", {
+      method: "POST",
+      body: JSON.stringify({ imageURL, promptValue }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/userGallery");
+    } else {
+      alert("Failed to create image");
+    }
+    console.log(response);
+  }
+}
+
+// TODO Send imageURL to SQL db
+async function todb() {
+  // user_name = req.session.user_name
+  // try {
+  //   const response = await fetch("/api/generateimage", {
+  //     method: "POST",
+  //     model: { Image },
+  //     attributes: ["prompt", "user_name", "imageURL"],
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       promptValue,
+  //       user_name,
+  //       imageURL,
+  //     }),
+  //   });
+  //   console.log(response);
+  //   if (!response.ok) {
+  //     throw new Error(
+  //       "Could not send image to personal gallery. Please try again!"
+  //     );
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  // }
+}
+
 // For dev work on resultModal, you can uncomment the line below
 //resultModal.show();
 
 // Listens for submit event
 document.querySelector("#submit").addEventListener("click", onSubmit);
 document.querySelector("#resultShare").addEventListener("click", publishBtn);
-document.querySelector("#resultSave").addEventListener("click", todb);
-
-// Listens for enter keydown
-document.querySelector("#submit").addEventListener("keypress", function(event){
-  if (event.key === "Enter") {
-    onSubmit;
-  }
-});
+document.querySelector("#resultSave").addEventListener("click", newImageHandler);
