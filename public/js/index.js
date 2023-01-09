0f28aefc3154849cad5e2bacc93aeaf40ac2b1ce
theLemmonade <lemmonmail@icloud.com>
2023-01-09 11:13:25 -0500

    bug fix

--- public/js/index.js
+++ public/js/index.js
@@ -6,15 +6,12 @@ const resultDate = document.querySelector("#resultDate");
 const resultShareIcon = document.querySelector("#resultShareIcon");
 const resultModal = new bootstrap.Modal(document.querySelector("#resultModal"),{ keyboard: false });
 const errModal = new bootstrap.Modal(document.querySelector("#errModal"), {keyboard: false,});
-// TODO assign these to lets to using session data
-let user_name = String;
 let date_created = String;
 let imageURL = String;
 let promptValue = String;
 let isPrivate = Boolean;
 imageURL = "./assets/1024x1024.png";
 isPrivate = true;
-console.log("prompt.js")
 
 // Hide spinner
 document.querySelector("#spinner").setAttribute("class", "spinner-border text-warning d-none");
@@ -24,7 +21,7 @@ function onSubmit(event) {
   event.preventDefault();
   console.log("onSubmit go");
   promptValue = promptInput.value;
-  console.log(promptValue);
+  console.log("prompt is " + promptValue);
   // If nothing is added then an alert pops up
   if (promptValue === "") {
     document
@@ -82,9 +79,6 @@ function configModal() {
   // set modal content from API response and session data
   resultImg.setAttribute("src", imageURL);
   resultPrompt.textContent = promptValue;
-  // TODO get session data and store under 'resultUser'
-  resultUser.textContent = user_name;
-  // TODO get current date and store under 'resultDate'
   resultDate.textContent = date_created;
 }
 
@@ -97,6 +91,25 @@ function beginModal() {
     .setAttribute("class", "spinner-border text-warning d-none");
   console.log("hiding spinner");
 }
+
+async function newImageHandler() {
+  if (imageURL && promptValue) {
+    const response = await fetch("/api/usergallery", {
+      method: "POST",
+      body: JSON.stringify({ imageURL, promptValue }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/userGallery");
+    } else {
+      alert("Failed to create image");
+    }
+    console.log(response);
+  }
+}
+
 
 // toggle isPrivate
 function publishBtn() {
@@ -117,19 +130,6 @@ function beginErrModal(error) {
   errModal.show();
 }
 
-// TODO Send imageURL to SQL db
-function todb() {
-  console.log("todb go");
-  // we dont need this if statement
-  if (!isPrivate) {
-    console.log("adding to gallery");
-    //
-  } else {
-    console.log("adding to Usergallery");
-    //
-  }
-}
-
 // For dev work on resultModal, you can uncomment the line below
 //resultModal.show();
 
@@ -141,6 +141,6 @@ document.querySelector("#resultSave").addEventListener("click", todb);
 // Listens for enter keydown
 document.querySelector("#submit").addEventListener("keypress", function(event){
   if (event.key === "Enter") {
-    document.querySelector("#submit").click();
+    onSubmit;
   }
 });
