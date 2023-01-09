fc09d2ceb0178882081a9db1ec639d382f4cb4ea
gitkcb <kcharlesbell@gmail.com>
2023-01-09 15:47:56 -0500

    photos over an hour old disappear

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,17 @@
+const moment = require("moment");
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
+    },
+    shouldRender: (date) => {
+let createdDate = moment(date).add(1, "hours");
+let currentDate = moment()
+if(createdDate > currentDate){
+  return true;
+}else{
+  return false;
+}
+
     }
 }
